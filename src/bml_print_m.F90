d42c92b51b9bd761078e2098abf35c2f4132c0f8
Nicolas Bock <nbock@lanl.gov>
2015-08-21 12:06:02 -0600

    Added utility function to print dense matrices.

--- src/bml_print_m.F90
+++ src/bml_print_m.F90
@@ -4,13 +4,20 @@
 module bml_print_m
   use bml_error_m
   implicit none
+
+  !> Print a matrix.
+  interface print_matrix
+     module procedure :: print_bml_matrix
+     module procedure :: print_dense_matrix
+  end interface print_matrix
+
 contains
 
   !> Print a matrix.
   !!
-  !! @param name A tag to be printed before the matrix.
-  !! @param A The matrix.
-  subroutine print_matrix(name, A)
+  !! \param name A tag to be printed before the matrix.
+  !! \param A The matrix.
+  subroutine print_bml_matrix(name, A)
 
     use bml_type_dense_m
     use bml_print_dense_m
@@ -28,8 +35,37 @@ contains
        class default
           call error(__FILE__, __LINE__, "unknown matrix type")
        end select
-    endif
+    end if
 
-  end subroutine print_matrix
+  end subroutine print_bml_matrix
+
+  !> Print a matrix.
+  !!
+  !! This is a utility function, that really does not belong into any
+  !! module here. It prints out a dense matrix.
+  !!
+  !! \param name A tag to be printed before the matrix.
+  !! \param A The matrix.
+  subroutine print_dense_matrix(name, A)
+
+    character(len=*), intent(in) :: name
+    double precision, allocatable, intent(in) :: A(:, :)
+
+    integer :: i, j
+    character(len=10000) :: line_format
+
+    if(.not. allocated(A)) then
+       write(*, "(A)") trim(name)//" not allocated"
+    else
+       write(*, "(A)") trim(adjustl(name))//" ="
+       write(line_format, *) size(A, 2)
+       line_format = trim(adjustl(line_format))
+       write(line_format, "(A)") "("//trim(line_format)//"es12.2)"
+       do i = 1, size(A, 1)
+          write(*, line_format) (A(i, j), j = 1, size(A, 2))
+       end do
+    end if
+
+  end subroutine print_dense_matrix
 
 end module bml_print_m
--- src/dense/bml_print_dense_m.F90
+++ src/dense/bml_print_dense_m.F90
@@ -3,13 +3,49 @@
 !> Print a dense matrix.
 module bml_print_dense_m
   implicit none
+
+  !> Print a matrix.
+  interface print_matrix_dense
+     module procedure print_matrix_dense_single
+     module procedure print_matrix_dense_double
+  end interface print_matrix_dense
+
 contains
 
   !> Print a matrix.
   !!
   !! @param name A tag to be printed before the matrix.
   !! @param A The matrix.
-  subroutine print_matrix_dense(name, A)
+  subroutine print_matrix_dense_single(name, A)
+
+    use bml_type_dense_m
+    use bml_error_m
+
+    character(len=*), intent(in) :: name
+    type(bml_matrix_dense_single_t), intent(in) :: A
+
+    integer :: i, j
+    character(len=10000) :: line_format
+
+    if(A%N > 20) then
+       call warning(__FILE__, __LINE__, "matrix is rather large")
+    else
+       write(*, "(A)") trim(adjustl(name))//" ="
+       write(line_format, *) A%N
+       line_format = trim(adjustl(line_format))
+       write(line_format, "(A)") "("//trim(line_format)//"es12.2)"
+       do i = 1, A%N
+          write(*, line_format) (A%matrix(i, j), j = 1, A%N)
+       end do
+    end if
+
+  end subroutine print_matrix_dense_single
+
+  !> Print a matrix.
+  !!
+  !! @param name A tag to be printed before the matrix.
+  !! @param A The matrix.
+  subroutine print_matrix_dense_double(name, A)
 
     use bml_type_dense_m
     use bml_error_m
@@ -29,9 +65,9 @@ contains
        write(line_format, "(A)") "("//trim(line_format)//"es12.2)"
        do i = 1, A%N
           write(*, line_format) (A%matrix(i, j), j = 1, A%N)
-       enddo
-    endif
+       end do
+    end if
 
-  end subroutine print_matrix_dense
+  end subroutine print_matrix_dense_double
 
 end module bml_print_dense_m
