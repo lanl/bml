e6bbaa543fea2b2259e106b9c0178f4fdf9bd512
Nicolas Bock <nbock@lanl.gov>
2015-08-25 06:21:28 -0600

    Add BLAS support for dense.

--- src/dense/bml_multiply_dense_m.F90
+++ src/dense/bml_multiply_dense_m.F90
@@ -3,6 +3,23 @@
 !> Matrix multiplication for dense matrices.
 module bml_multiply_dense_m
   implicit none
+
+  !> Interface to BLAS {s,d}gemm functions.
+  interface
+     subroutine dgemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)
+       double precision :: alpha, beta
+       integer :: k, lda, ldb, ldc, m, n
+       character :: transa, transb
+       double precision :: a(lda,*), b(ldb,*), c(ldc,*)
+     end subroutine dgemm
+     subroutine sgemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)
+       real :: alpha, beta
+       integer :: k, lda, ldb, ldc, m, n
+       character :: transa, transb
+       real :: a(lda,*), b(ldb,*), c(ldc,*)
+     end subroutine sgemm
+  end interface
+
 contains
 
   !> Multiply two matrices.
@@ -23,7 +40,11 @@ contains
     double precision, intent(in) :: alpha
     double precision, intent(in) :: beta
 
+#ifdef BLAS_FOUND
+    call dgemm("N", "N", A%N, A%N, A%N, alpha, A%matrix, A%N, B%matrix, A%N, beta, C%matrix, A%N)
+#else
     C%matrix = alpha*matmul(A%matrix, B%matrix)+beta*C%matrix
+#endif
 
   end subroutine multiply_dense
 
