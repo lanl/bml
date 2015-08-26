8307d8c5b1e7efbad322617d6cfd37b00f081e29
Nicolas Bock <nbock@lanl.gov>
2015-08-26 10:23:00 -0600

    Add build convenience script.

---
+++ scripts/full-test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+BUILD_DIR=${PWD}/build
+INSTALL_DIR=${PWD}/install
+
+mkdir "${BUILD_DIR}" || exit
+mkdir "${INSTALL_DIR}" || exit
+
+cd "${BUILD_DIR}"
+cmake .. \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_Fortran_COMPILER=${FC:=gfortran} \
+  -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+  -DBML_TESTING=yes
+
+make || exit
+make doc || exit
+make test || exit
+make install || exit
