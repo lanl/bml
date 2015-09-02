dfb104fd4f69996cd63b4316e8eb511e61ac196f
Nicolas Bock <nbock@lanl.gov>
2015-09-02 10:08:04 -0600

    Generified the test driver.

--- tests/test_driver.F90
+++ tests/test_driver.F90
@@ -10,26 +10,8 @@ program test
   integer, parameter :: N = 129
   type(TEST_TYPE) :: tester
 
-  write(*, "(A)") "Testing "//BML_MATRIX_DENSE//":"//BML_PRECISION_SINGLE
-  if(.not. tester%test_function(N, BML_MATRIX_DENSE, BML_PRECISION_SINGLE)) then
-     write(*, "(A)") "Test failed"
-     error stop
-  end if
-
-  write(*, "(A)") "Testing "//BML_MATRIX_DENSE//":"//BML_PRECISION_DOUBLE
-  if(.not. tester%test_function(N, BML_MATRIX_DENSE, BML_PRECISION_DOUBLE)) then
-     write(*, "(A)") "Test failed"
-     error stop
-  end if
-
-  write(*, "(A)") "Testing "//BML_MATRIX_ELLPACK//":"//BML_PRECISION_SINGLE
-  if(.not. tester%test_function(N, BML_MATRIX_ELLPACK, BML_PRECISION_SINGLE)) then
-     write(*, "(A)") "Test failed"
-     error stop
-  end if
-
-  write(*, "(A)") "Testing "//BML_MATRIX_ELLPACK//":"//BML_PRECISION_DOUBLE
-  if(.not. tester%test_function(N, BML_MATRIX_ELLPACK, BML_PRECISION_DOUBLE)) then
+  write(*, "(A)") "Testing "//MATRIX_TYPE//":"//PRECISION
+  if(.not. tester%test_function(N, MATRIX_TYPE, PRECISION)) then
      write(*, "(A)") "Test failed"
      error stop
   end if
