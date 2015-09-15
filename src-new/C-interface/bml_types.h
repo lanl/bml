40f3ff069db73706746cc843f080f89e3d8295c9
Nicolas Bock <nbock@lanl.gov>
2015-09-15 11:25:37 -0600

    Adding private types declarations.

--- src-new/C-interface/bml_types.h
+++ src-new/C-interface/bml_types.h
@@ -3,9 +3,6 @@
 #ifndef __BML_TYPES_H
 #define __BML_TYPES_H
 
-/** The matrix type. */
-typedef void bml_matrix_t;
-
 /** The supported matrix types. */
 typedef enum {
     /** The matrix is not initialized. */
@@ -25,5 +22,8 @@ typedef enum {
     /** Matrix data is stored in double precision (double). */
     double_precision
 } bml_matrix_precision_t;
+
+/** The matrix type. */
+typedef void bml_matrix_t;
 
 #endif
