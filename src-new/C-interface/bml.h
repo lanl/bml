db6692f085ed51e5d1f1a93bb900d662499077ea
Nicolas Bock <nbock@lanl.gov>
2015-09-18 11:43:19 -0600

    Updated documentation.

--- src-new/C-interface/bml.h
+++ src-new/C-interface/bml.h
@@ -8,47 +8,19 @@
  *   - ellpack (sparse)
  *   - csr (sparse)
  *
- * \section fortran_usage Fortran Usage
+ * \section usage_examples Usage Examples
  *
- * The use of this library is pretty straightforward. In the
- * application code, `use` the bml main module,
+ * Usage examples can be found here:
+ *   - \ref fortran_usage "Fortran Usage"
+ *   - \ref C_usage "C Usage"
  *
- * \code{.F90}
- * use bml
- * \endcode
+ * \section developers Modifying the library itself
  *
- * A matrix is of type
+ * If you are interested in modifying the library code itself, please
+ * have a look at the
+ * \ref developer_documentation "Developer Documentation".
  *
- * \code{.F90}
- * type(bml_matrix_t) :: a
- * \endcode
- *
- * There are two important things to note. First, although not
- * explicitly state in the above example, the matrix is not yet
- * allocated. Hence, the matrix needs to be allocated through an
- * allocation procedure with the desired type and precision,
- * e.g. dense:double, see the page on \ref allocate_group_Fortran
- * "allocation functions" for a complete list. For instance,
- *
- * \code{.F90}
- * call bml_zero_matrix(BML_MATRIX_DENSE, BML_PRECISION_DOUBLE, 100, a)
- * \endcode
- *
- * will allocate a dense, double-precision, \f$ 100 \times 100 \f$
- * matrix which is initialized to zero. Additional functions allocate
- * special matrices,
- *   - bml_allocate::bml_random_matrix Allocate and initialize a
- *     random matrix.
- *   - bml_allocate::bml_identity_matrix Allocate and initialize the
- *     identity matrix.
- *
- * A matrix is deallocated by calling
- *
- * \code{.F90}
- * call bml_deallocate(a)
- * \endcode
- *
- * \subsection supported_type Supported Matrix Types
+ * \section supported_type Supported Matrix Types
  *
  * Support types:
  *     - bml_matrix_t
@@ -111,17 +83,6 @@
  *         - bml_commutator::bml_commutator
  *         - bml_commutator::bml_anticommutator
  *
- * \section C_usage C Usage
- *
- * In C, the following example code does the same as the above Fortran code:
- *
- * \code{.c}
- * #include <bml.h>
- *
- * bml_matrix_t *A = bml_zero_matrix(dense, single_real, 100);
- * bml_deallocate(&A);
- * \endcode
- *
  * \author Jamaludin Mohd-Yusof <jamal@lanl.gov>
  * \author Nicolas Bock <nbock@lanl.gov>
  * \author Susan M. Mniszewski <smm@lanl.gov>
@@ -132,6 +93,68 @@
  * \defgroup convert_group_C Converting between Matrix Formats (C interface)
  * \defgroup allocate_group_Fortran Allocation and Deallocation Functions (Fortran interface)
  * \defgroup convert_group_Fortran Converting between Matrix Formats (Fortran interface)
+ */
+
+/** \page C_usage C Usage
+ *
+ * In C, the following example code does the same as the above Fortran code:
+ *
+ * \code{.c}
+ * #include <bml.h>
+ *
+ * bml_matrix_t *A = bml_zero_matrix(dense, single_real, 100);
+ * bml_deallocate(&A);
+ * \endcode
+ *
+ * Back to the \ref index "main page".
+ */
+
+/** \page fortran_usage Fortran Usage
+ *
+ * The use of this library is pretty straightforward. In the
+ * application code, `use` the bml main module,
+ *
+ * \code{.F90}
+ * use bml
+ * \endcode
+ *
+ * A matrix is of type
+ *
+ * \code{.F90}
+ * type(bml_matrix_t) :: a
+ * \endcode
+ *
+ * There are two important things to note. First, although not
+ * explicitly state in the above example, the matrix is not yet
+ * allocated. Hence, the matrix needs to be allocated through an
+ * allocation procedure with the desired type and precision,
+ * e.g. dense:double, see the page on \ref allocate_group_Fortran
+ * "allocation functions" for a complete list. For instance,
+ *
+ * \code{.F90}
+ * call bml_zero_matrix(BML_MATRIX_DENSE, BML_PRECISION_DOUBLE, 100, a)
+ * \endcode
+ *
+ * will allocate a dense, double-precision, \f$ 100 \times 100 \f$
+ * matrix which is initialized to zero. Additional functions allocate
+ * special matrices,
+ *   - bml_allocate::bml_random_matrix Allocate and initialize a
+ *     random matrix.
+ *   - bml_allocate::bml_identity_matrix Allocate and initialize the
+ *     identity matrix.
+ *
+ * A matrix is deallocated by calling
+ *
+ * \code{.F90}
+ * call bml_deallocate(a)
+ * \endcode
+ *
+ * Back to the \ref index "main page".
+ */
+
+/** \page developer_documentation Developer Documentation
+ *
+ * Back to the \ref index "main page".
  */
 
 /** \copyright Los Alamos National Laboratory 2015 */
