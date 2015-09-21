09e451f93e3373c0f8eff6ca6021d098d819519b
Susan.Mniszewski <smm@lanl.gov>
2015-09-21 11:22:58 -0600

    Changed copy test.

--- tests-new/copy_matrix.c
+++ tests-new/copy_matrix.c
@@ -11,48 +11,61 @@ int test_function(const int N,
 {
     bml_matrix_t *A = NULL;
     bml_matrix_t *B = NULL;
+    bml_matrix_t *C = NULL;
 
-    float *A_float = NULL, *B_float = NULL;
-    double *A_double = NULL, *B_double = NULL;
+    float *A_float = NULL, *B_float = NULL, *C_float = NULL;
+    double *A_double = NULL, *B_double = NULL, *C_double = NULL;
 
     A = bml_random_matrix(matrix_type, matrix_precision, N, M);
-    B = bml_copy(A);
+    //B = bml_zero_matrix(matrix_type, matrix_precision, N, M);
+    //bml_copy(A, B);
+    B = bml_copy_new(A);
+    C = bml_zero_matrix(matrix_type, matrix_precision, N, M);
+    bml_copy(B, C);
 
     switch(matrix_precision) {
     case single_real:
         A_float = bml_convert_to_dense(A);
         B_float = bml_convert_to_dense(B);
+        C_float = bml_convert_to_dense(C);
         bml_print_matrix(N, matrix_precision, A_float, 0, N, 0, N);
         bml_print_matrix(N, matrix_precision, B_float, 0, N, 0, N);
-       for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                if(fabs(A_float[i+j*N] - B_float[i+j*N]) > 1e-12) {
-                    LOG_ERROR("matrices are not identical; A[%d,%d] = %e\n", i, j, A_float[i*N+j]);
-                    return -1;
-                }
+        bml_print_matrix(N, matrix_precision, C_float, 0, N, 0, N);
+       for(int i = 0; i < N*N; i++) {
+            if(fabs(A_float[i] - B_float[i]) > 1e-12 ||
+               fabs(A_float[i] - C_float[i]) > 1e-12) {
+               LOG_ERROR("matrices are not identical; A[%d] = %e\n", i, A_float[i]);
+               return -1;
             }
         }
         bml_free_memory(A_float);
         bml_free_memory(B_float);
+        bml_free_memory(C_float);
         break;
     case double_real:
         A_double = bml_convert_to_dense(A);
         B_double = bml_convert_to_dense(B);
+        C_double = bml_convert_to_dense(C);;
         bml_print_matrix(N, matrix_precision, A_double, 0, N, 0, N);
         bml_print_matrix(N, matrix_precision, B_double, 0, N, 0, N);
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                if(fabs(A_double[i+j*N] - B_double[i+j*N]) > 1e-12) {
-                    LOG_ERROR("matrices are not identical; A[%d,%d] = %e\n", i, j, A_double[i*N+j]);
-                    return -1;
-                }
+        bml_print_matrix(N, matrix_precision, C_double, 0, N, 0, N);
+        for(int i = 0; i < N*N; i++) {
+            if(fabs(A_double[i] - B_double[i]) > 1e-12 ||
+               fabs(A_double[i] - C_double[i]) > 1e-12) {
+                LOG_ERROR("matrices are not identical; A[%d] = %e\n", i, A_double[i]);
+                return -1;
             }
         }
         bml_free_memory(A_double);
         bml_free_memory(B_double);
+        bml_free_memory(C_double);
         break;
     }
-    LOG_INFO("identity matrix test passed\n");
+    bml_deallocate(&A);
+    bml_deallocate(&B);
+    bml_deallocate(&C);
+
+    LOG_INFO("copy matrix test passed\n");
 
     return 0;
 }
