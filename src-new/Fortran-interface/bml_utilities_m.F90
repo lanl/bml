d291678b1d1de33aac2ca770539d4425956099bf
Nicolas Bock <nbock@lanl.gov>
2015-09-30 14:53:23 -0600

    Added bml vectors.

--- src-new/Fortran-interface/bml_utilities_m.F90
+++ src-new/Fortran-interface/bml_utilities_m.F90
@@ -11,6 +11,15 @@ module bml_utilities_m
   private
 
   interface
+
+     subroutine bml_print_bml_vector_C(v, i_l, i_u) &
+          bind(C, name="bml_print_bml_vector")
+       use, intrinsic :: iso_C_binding
+       type(C_PTR), value, intent(in) :: v
+       integer(C_INT), value, intent(in) :: i_l
+       integer(C_INT), value, intent(in) :: i_u
+     end subroutine bml_print_bml_vector_C
+
      subroutine bml_print_bml_matrix_C(a, i_l, i_u, j_l, j_u) &
           bind(C, name="bml_print_bml_matrix")
        use, intrinsic :: iso_C_binding
@@ -20,7 +29,12 @@ module bml_utilities_m
        integer(C_INT), value, intent(in) :: j_l
        integer(C_INT), value, intent(in) :: j_u
      end subroutine bml_print_bml_matrix_C
+
   end interface
+
+  interface bml_print_vector
+     module procedure bml_print_bml_vector
+  end interface bml_print_vector
 
   interface bml_print_matrix
      module procedure bml_print_bml_matrix
@@ -30,7 +44,7 @@ module bml_utilities_m
 
 contains
 
-  !> Print a dense matrix.
+  !> Print a bml matrix.
   !!
   !! \param tag A string to print before the matrix.
   !! \param a The matrix.
@@ -53,5 +67,25 @@ contains
     call bml_print_bml_matrix_C(c_loc(a), i_l, i_u, j_l, j_u)
 
   end subroutine bml_print_bml_matrix
+
+  !> Print a bml vector.
+  !!
+  !! \param tag A string to print before the matrix.
+  !! \param v The vector.
+  !! \param i_l The lower row bound.
+  !! \param i_u The upper row bound.
+  subroutine bml_print_bml_vector(tag, v, i_l, i_u)
+
+    use, intrinsic :: iso_C_binding
+    use bml_types_m
+
+    character(len=*), intent(in) :: tag
+    type(bml_vector_t), target, intent(in) :: v
+    integer, intent(in) :: i_l
+    integer, intent(in) :: i_u
+
+    call bml_print_bml_vector_C(c_loc(v), i_l, i_u)
+
+  end subroutine bml_print_bml_vector
 
 end module bml_utilities_m
