0c7fa5aa3b2bd9fd70cbe06cf33f704e3b09dbea
Nicolas Bock <nbock@lanl.gov>
2015-11-16 12:35:21 -0700

    Updated indent.sh script

--- indent.sh
+++ indent.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-indent -gnu -nut -i4 -bli0 -cli4 -ppi0 -cbi0 -npcs -bfda $*
+INDENT_ARGS="-gnu -nut -i4 -bli0 -cli4 -ppi0 -cbi0 -npcs -bfda"
+
+if [[ $# -gt 0 ]]; then
+    indent ${INDENT_ARGS} $*
+else
+    BASEDIR="$(dirname $0)"
+    indent ${INDENT_ARGS} \
+           "${BASEDIR}"/src/C-interface/{,dense,ellpack}/*.{c,h} \
+           "${BASEDIR}"/tests/*.{c,h}
+fi
