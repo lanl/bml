7c895c5765e3d29fdd580bce52d8a7db5e3dcc4c
Christian Negre <cnegre@lanl.gov>
2017-06-21 14:30:23 -0600

    Sparsity added

--- src/C-interface/dense/bml_introspection_dense.c
+++ src/C-interface/dense/bml_introspection_dense.c
@@ -114,7 +114,7 @@ bml_get_row_bandwidth_dense(
     return -1;
 }
 
-/** Return the bandwidth of a row in the matrix.
+/** Return the bandwidth of a matrix.
  *
  * \param A The bml matrix.
  * \return The bandwidth of row i.
@@ -136,6 +136,47 @@ bml_get_bandwidth_dense(
             break;
         case double_complex:
             return bml_get_bandwidth_dense_double_complex(A);
+            break;
+        case precision_uninitialized:
+            LOG_ERROR("precision not initialized");
+            break;
+        default:
+            LOG_ERROR("fatal logic error\n");
+            break;
+    }
+    return -1;
+}
+
+/** Return the sparsity of a bml matrix.
+ *
+ *  Note that the the sparsity of a matrix is defined
+ *  as NumberOfZeroes/N*N where N is the matrix dimension.
+ *  The density of matrix A will be defined as 1-sparsity(A)
+ *
+ * \ingroup introspection_group_C
+ *
+ * \param A The bml matrix.
+ * \param threshold The threshold used to compute the sparsity.
+ * \return The sparsity of A.
+ */
+double
+bml_get_sparsity_dense(
+    const bml_matrix_dense_t * A,
+    const double threshold)
+{
+    switch (bml_get_precision_dense(A))
+    {
+        case single_real:
+            return bml_get_sparsity_dense_single_real(A, threshold);
+            break;
+        case double_real:
+            return bml_get_sparsity_dense_double_real(A, threshold);
+            break;
+        case single_complex:
+            return bml_get_sparsity_dense_single_complex(A, threshold);
+            break;
+        case double_complex:
+            return bml_get_sparsity_dense_double_complex(A, threshold);
             break;
         case precision_uninitialized:
             LOG_ERROR("precision not initialized");
