90064b646b4ba5a04664e0661a55e83d79ee51a1
Christian Negre <cnegre@lanl.gov>
2017-06-21 15:12:02 -0600

    added typed

---
+++ src/C-interface/ellsort/bml_introspection_ellsort_typed.c
@@ -0,0 +1,54 @@
+#include "../macros.h"
+#include "../typed.h"
+#include "../bml_introspection.h"
+#include "bml_introspection_ellsort.h"
+#include "bml_types_ellsort.h"
+#include "bml_types.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <complex.h>
+#include <math.h>
+
+/** Return the sparsity of a matrix.
+ *
+ *  Note that the the sparsity of a matrix is defined
+ *  as NumberOfZeroes/N*N where N is the matrix dimension.
+ *  To density of matrix A will be defined as 1-sparsity(A)
+ *
+ * \ingroup introspection_group_C
+ *
+ * \param A The bml matrix.
+ * \param threshold The threshold used to compute the sparsity.
+ * \return The sparsity of A.
+ */
+double TYPED_FUNC(
+    bml_get_sparsity_ellsort) (
+    const bml_matrix_ellsort_t * A,
+    const double threshold)
+{
+    int nnzs = 0;
+    int i;
+    int j;
+    double sparsity;
+    REAL_T *A_value = (REAL_T *) A->value;
+    int A_N = A->N;
+    int A_M = A->M;
+    int *A_nnz = A->nnz;
+
+    for (int i = 0; i < A->N; i++)
+    {
+        // printf("Annz,%d\n", A_nnz[i]);
+        for (int j = 0; j < A_nnz[i]; j++)
+            // printf("Avalue,%f\n", A_value[ROWMAJOR(i, j,A_nnz[i], A_N)]);
+            if (ABS(A_value[ROWMAJOR(i, j, A_nnz[i], A_N)]) > threshold)
+            {
+                nnzs++;
+            }
+    }
+
+    sparsity = (1.0 - (double) nnzs / ((double) (A_N * A_N)));
+
+    return sparsity;
+
+}
