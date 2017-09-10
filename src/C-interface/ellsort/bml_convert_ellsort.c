03fb87e959e84f445e96081eced574f12e43f05b
Nicolas Bock <nicolasbock@gmail.com>
2017-09-10 06:30:07 -0600

    Turn on `check_indent` test
    Turn indentation checks back on. The check was accidentally removed in
    7e16a1301f67f805478aadff12a966ad0cfccd9c.
    
    This change fixes incorrect indentation in some files.

--- src/C-interface/ellsort/bml_convert_ellsort.c
+++ src/C-interface/ellsort/bml_convert_ellsort.c
@@ -13,16 +13,20 @@ bml_convert_ellsort(
     switch (matrix_precision)
     {
         case single_real:
-            return bml_convert_ellsort_single_real(A, matrix_precision, M, distrib_mode);
+            return bml_convert_ellsort_single_real(A, matrix_precision, M,
+                                                   distrib_mode);
             break;
         case double_real:
-            return bml_convert_ellsort_double_real(A, matrix_precision, M, distrib_mode);
+            return bml_convert_ellsort_double_real(A, matrix_precision, M,
+                                                   distrib_mode);
             break;
         case single_complex:
-            return bml_convert_ellsort_single_complex(A, matrix_precision, M, distrib_mode);
+            return bml_convert_ellsort_single_complex(A, matrix_precision, M,
+                                                      distrib_mode);
             break;
         case double_complex:
-            return bml_convert_ellsort_double_complex(A, matrix_precision, M, distrib_mode);
+            return bml_convert_ellsort_double_complex(A, matrix_precision, M,
+                                                      distrib_mode);
             break;
         default:
             LOG_ERROR("unknown precision\n");
