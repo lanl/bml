bb3e239f660dd0d9db6bdc0cfd7a39ffdfe77976
Nicolas Bock <nicolasbock@gmail.com>
2017-09-11 04:42:34 -0600

    Fix indentation

--- tests/test_backtrace.c
+++ tests/test_backtrace.c
@@ -1,12 +1,17 @@
 #include "bml_logger.h"
 
-void foo(void)
+void
+foo(
+    void)
 {
     LOG_DEBUG("Test function\n");
     LOG_ERROR("Print backtrace\n");
 }
 
-int main (int argc, char **argv)
+int
+main(
+    int argc,
+    char **argv)
 {
     LOG_INFO("Starting test...\n");
     foo();
