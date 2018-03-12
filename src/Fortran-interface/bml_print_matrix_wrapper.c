3d5be2904cd8a52ba5d248f39679a7f56a8aac42
Nicolas Bock <nicolasbock@gmail.com>
2018-03-12 06:29:33 -0600

    Indent previously unchecked files
    After the previous change we are checking more files. This change
    indents those additional files correctly.

--- src/Fortran-interface/bml_print_matrix_wrapper.c
+++ src/Fortran-interface/bml_print_matrix_wrapper.c
@@ -1,21 +1,25 @@
 #include "bml_utilities.h"
 
-void bml_print_matrix_single(const int N,
-                             const float *A,
-                             const int i_l,
-                             const int i_u,
-                             const int j_l,
-                             const int j_u)
+void
+bml_print_matrix_single(
+    const int N,
+    const float *A,
+    const int i_l,
+    const int i_u,
+    const int j_l,
+    const int j_u)
 {
     bml_print_dense_matrix(N, single_real, A, i_l, i_u, j_l, j_u);
 }
 
-void bml_print_matrix_double(const int N,
-                             const double *A,
-                             const int i_l,
-                             const int i_u,
-                             const int j_l,
-                             const int j_u)
+void
+bml_print_matrix_double(
+    const int N,
+    const double *A,
+    const int i_l,
+    const int i_u,
+    const int j_l,
+    const int j_u)
 {
     bml_print_dense_matrix(N, double_real, A, i_l, i_u, j_l, j_u);
 }
