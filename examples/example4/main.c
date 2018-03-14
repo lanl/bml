6370b630f8b88943d758eeb348de7b7511ac9867
christian <Christian Negre>
2018-03-13 20:42:54 -0600

    Fixing codacy issues

--- examples/example4/main.c
+++ examples/example4/main.c
@@ -8,26 +8,22 @@
 
 int main ()
 {
-    bml_matrix_t *A = NULL;
-    bml_matrix_t *B = NULL;
-    bml_matrix_t *C = NULL;
+    bml_matrix_t *A;
+    bml_matrix_t *B;
+    bml_matrix_t *C;
     
-    const int N = 5000;
-    const int M = 5000;
+    const int N = 1000;
+    const int M = 1000;
     bml_matrix_precision_t matrix_precision = double_real;
     bml_matrix_type_t matrix_type = dense;
     const double alpha = 1.2;
     const double beta = 0.8;
     const double threshold = 0.0;
-    double *trace;
+    void *trace;
 
     A = bml_random_matrix(matrix_type, matrix_precision, N, M, sequential);
     B = bml_random_matrix(matrix_type, matrix_precision, N, M, sequential);
     C = bml_random_matrix(matrix_type, matrix_precision, N, M, sequential);
-
-//    bml_print_bml_matrix(A, 0, N, 0, N);
-//    bml_print_bml_matrix(B, 0, N, 0, N);
-//    bml_print_bml_matrix(C, 0, N, 0, N);
 
     trace = bml_multiply_x2(A, C, threshold); // C=A*A
     bml_multiply(A, B, C, alpha, beta, threshold); // C=alpha* A*B + beta*C
