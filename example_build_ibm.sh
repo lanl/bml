381db681aecdf0201f1a9c0a2e7730fbc2b6100d
cnegre <christianfannegre@gmail.com>
2019-02-14 12:37:38 -0500

    example_build_ibm.sh

--- example_build_ibm.sh
+++ example_build_ibm.sh
@@ -9,7 +9,6 @@ MY_PATH=$(pwd)
 export ESSL_DIR=${ESSL_DIR:="${OLCF_ESSL_ROOT}"}
 export CC=${CC:=xlC_r}
 export FC=${FC:=xlf90_r}
-#export FC=${FC:=mpif90}
 export CXX=${CXX:=xlC_r}
 export BLAS_VENDOR=${BLAS_VENDOR:=IBM}
 export BML_OPENMP=${BML_OPENMP:=yes}
@@ -20,6 +19,7 @@ export EXTRA_CFLAGS=${EXTRA_CFLAGS:="-qsmp=omp"}
 export CMAKE_Fortran_FLAGS=${CMAKE_Fortran_FLAGS:="-qxlf2003=polymorphic -qthreaded"}
 export CMAKE_Fortran_FLAGS=${CMAKE_Fortran_FLAGS:="-qxlf2003=polymorphic -qthreaded -L/sw/summitdev/essl/6.1.0/essl/6.1/lib64 -lessl -lesslsmp "}
 export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:="-qsmp=omp"}
+
 
 
 ./build.sh configure
