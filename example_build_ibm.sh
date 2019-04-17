4bba3c884b6f15231a59a56a8dedfc52fdfeffd9
cnegre <christianfannegre@gmail.com>
2019-04-17 17:47:59 -0400

    deleted lines in example_build_ibm.sh

--- example_build_ibm.sh
+++ example_build_ibm.sh
@@ -6,11 +6,10 @@ rm -r build
 rm -r install
 
 MY_PATH=$(pwd)
-#export ESSL_DIR=${ESSL_DIR:=/sw/summitdev/essl/5.5.0-20161110}
-#export ESSL_DIR=${ESSL_DIR:=/sw/summitdev/essl/5.5.0-20161110}
-export ESSL_DIR=${ESSL_DIR:=/sw/summitdev/essl/6.1.0/essl/6.1/}
+export ESSL_DIR=${ESSL_DIR:="${OLCF_ESSL_ROOT}"}
 export CC=${CC:=xlC_r}
-export FC=${FC:=/sw/summitdev/xl/16.1.1-beta6/xlf/16.1.1/bin/xlf90_r}
+export FC=${FC:=xlf90_r}
+#export FC=${FC:=mpif90}
 export CXX=${CXX:=xlC_r}
 export BLAS_VENDOR=${BLAS_VENDOR:=IBM}
 export BML_OPENMP=${BML_OPENMP:=yes}
@@ -21,10 +20,6 @@ export EXTRA_CFLAGS=${EXTRA_CFLAGS:="-qsmp=omp"}
 export CMAKE_Fortran_FLAGS=${CMAKE_Fortran_FLAGS:="-qxlf2003=polymorphic -qthreaded"}
 export CMAKE_Fortran_FLAGS=${CMAKE_Fortran_FLAGS:="-qxlf2003=polymorphic -qthreaded -L/sw/summitdev/essl/6.1.0/essl/6.1/lib64 -lessl -lesslsmp "}
 export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:="-qsmp=omp"}
-
-
-
-#CC=xlc_r FC=xlf_r BLAS_VENDOR=IBM CMAKE_BUILD_TYPE=Release BML_OPENMP=yes CMAKE_INSTALL_PREFIX=/home/smm/bml/bml/install CMAKE_Fortran_FLAGS="-qxlf2003=polymorphic -qthreaded" ./build.sh configure
 
 
 ./build.sh configure
