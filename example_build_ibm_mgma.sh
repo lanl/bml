ddec1700a42b2bf48ca12fe92092a91c6bab5a53
cnegre <christianfannegre@gmail.com>
2019-05-03 17:02:44 -0400

    example build ibm magma

--- example_build_ibm_mgma.sh
+++ example_build_ibm_mgma.sh
@@ -6,8 +6,6 @@ rm -r build
 rm -r install
 
 MY_PATH=$(pwd)
-#export ESSL_DIR=${ESSL_DIR:=/sw/summitdev/essl/5.5.0-20161110}
-#export ESSL_DIR=${ESSL_DIR:=/sw/summitdev/essl/5.5.0-20161110}
 export ESSL_DIR=${ESSL_DIR:="${OLCF_ESSL_ROOT}"}
 export OLCF_MAGMA_ROOT=${OLCF_MAGMA_ROOT:="${MEMBERWORK}/csc304/magma-2.5.0"}
 export CC=${CC:=xlC_r}
@@ -22,13 +20,8 @@ export MAGMA_ROOT=$OLCF_MAGMA_ROOT
 export BML_MAGMA=${BML_MAGMA:=yes}
 export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Release}
 export EXTRA_CFLAGS=${EXTRA_CFLAGS:="-qsmp=omp"}
-#export CMAKE_Fortran_FLAGS=${CMAKE_Fortran_FLAGS:="-qxlf2003=polymorphic -qthreaded"}
 export CMAKE_Fortran_FLAGS=${CMAKE_Fortran_FLAGS:="-qxlf2003=polymorphic -qthreaded -L${ESSL_DIR}/lib64 -lessl -lesslsmp "}
 export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:="-qsmp=omp"}
-
-
-
-#CC=xlc_r FC=xlf_r BLAS_VENDOR=IBM CMAKE_BUILD_TYPE=Release BML_OPENMP=yes CMAKE_INSTALL_PREFIX=/home/smm/bml/bml/install CMAKE_Fortran_FLAGS="-qxlf2003=polymorphic -qthreaded" ./build.sh configure
 
 
 ./build.sh configure
