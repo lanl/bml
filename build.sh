3f38bf6c7bf605f36215b3e901cbad09ff0f1292
Nicolas Bock <nicolasbock@gmail.com>
2019-06-26 23:51:49 +0200

    Add coverage flag
    The coverage compiler option is only used for the `coverage` stage.
    This change makes the `--coverage` compiler flag optional.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- build.sh
+++ build.sh
@@ -50,6 +50,7 @@ EOF
     echo "BML_COMPLEX            {yes,no}                    (default is ${BML_COMPLEX})"
     echo "BML_TESTING            {yes,no}                    (default is ${BML_TESTING})"
     echo "BML_VALGRIND           {yes,no}                    (default is ${BML_VALGRIND})"
+    echo "BML_COVERAGE           {yes,no}                    (default is ${BML_COVERAGE})"
     echo "BUILD_DIR              Path to build dir           (default is ${BUILD_DIR})"
     echo "BLAS_VENDOR            {,Intel,MKL,ACML,GNU,IBM,Auto}  (default is '${BLAS_VENDOR}')"
     echo "BML_INTERNAL_BLAS      {yes,no}                    (default is ${BML_INTERNAL_BLAS})"
@@ -86,6 +87,7 @@ set_defaults() {
     : ${CMAKE_Fortran_FLAGS:=}
     : ${BML_TESTING:=yes}
     : ${BML_VALGRIND:=no}
+    : ${BML_COVERAGE:=no}
     : ${FORTRAN_FLAGS:=}
     : ${EXTRA_LINK_FLAGS:=}
     : ${BML_GPU:=no}
@@ -151,6 +153,7 @@ configure() {
         -DBUILD_SHARED_LIBS="${BUILD_SHARED_LIBS}" \
         -DBML_TESTING="${BML_TESTING:=yes}" \
         -DBML_VALGRIND="${BML_VALGRIND:=no}" \
+        -DBML_COVERAGE="${BML_COVERAGE:=no}" \
         -DBLAS_VENDOR="${BLAS_VENDOR}" \
         -DBML_INTERNAL_BLAS="${BML_INTERNAL_BLAS}" \
         ${EXTRA_CFLAGS:+-DEXTRA_CFLAGS="${EXTRA_CFLAGS}"} \
