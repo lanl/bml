466e4c570d6db8101e0295b599bbb4f081d18147
Nicolas Bock <nicolasbock@gmail.com>
2019-07-07 23:14:40 +0200

    Add Fortran related indentation settings
    This change adds more settings for the Emacs editor for Fortran
    related indentation settings.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,8 @@
 
 ((c-mode
   ((c-basic-offset . 4)
-   (c-offsets-alist . ((arglist-intro . 4))))))
+   (c-offsets-alist . ((arglist-intro . 4)))))
+ (f90-mode
+  ((f90-if-indent . 2)
+   (f90-do-indent . 2)
+   (f90-type-indent . 2))))
