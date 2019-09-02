ae3cb5a45138fa6b8db12f5dd66f9c39fe643b44
cnegre <christianfannegre@gmail.com>
2019-09-02 13:58:22 -0400

    changed path

--- example_build_gnu_openblass_mgma.sh
+++ example_build_gnu_openblass_mgma.sh
@@ -15,7 +15,7 @@ export INSTALL_DIR=${INSTALL_DIR:="${MY_PATH}/install"}
 export BML_TESTING=${BML_TESTING:=yes}
 export BML_COMPLEX=${BML_COMPLEX:=yes}
 export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Release}
-export MAGMA_ROOT=${MAGMA_ROOT:="${MEMBERWORK}/csc304/magma-2.5.0"}
+export MAGMA_ROOT=${MAGMA_ROOT:="${HOME}/magma-2.5.1"}
 export BML_MAGMA=${BML_MAGMA:=yes}
 export EXTRA_CFLAGS=${EXTRA_CFLAGS:="-fopenmp -lpthread -I${OLCF_OPENBLAS_ROOT}/include "}
 export CMAKE_Fortran_FLAGS=${CMAKE_Fortran_FLAGS:="-fopenmp -lpthread -L${OLCF_OPENBLAS_ROOT}/lib64 -lopenblas -L${OLCF_CUDA_ROOT}/lib64/ -lcublas -lcudart"}
