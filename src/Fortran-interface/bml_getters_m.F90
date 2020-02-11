d2e98805072db580807b5fa7ce73cac1435b4c19
Nicolas Bock <nicolasbock@gmail.com>
2020-02-11 15:09:28 -0700

    Fix memory leak for getter
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- src/Fortran-interface/bml_getters_m.F90
+++ src/Fortran-interface/bml_getters_m.F90
@@ -41,6 +41,7 @@ contains
     ptr = bml_get_diagonal_C(a%ptr)
     call c_f_pointer(ptr, diagonal_ptr, [bml_get_N(a)])
     diagonal = diagonal_ptr
+    deallocate(diagonal_ptr)
 
   end subroutine bml_get_diagonal_single_real
 
@@ -59,6 +60,7 @@ contains
     ptr = bml_get_diagonal_C(a%ptr)
     call c_f_pointer(ptr, diagonal_ptr, [bml_get_N(a)])
     diagonal = diagonal_ptr
+    deallocate(diagonal_ptr)
 
   end subroutine bml_get_diagonal_double_real
 
@@ -77,6 +79,7 @@ contains
     ptr = bml_get_diagonal_C(a%ptr)
     call c_f_pointer(ptr, diagonal_ptr, [bml_get_N(a)])
     diagonal = diagonal_ptr
+    deallocate(diagonal_ptr)
 
   end subroutine bml_get_diagonal_single_complex
 
@@ -95,6 +98,7 @@ contains
     ptr = bml_get_diagonal_C(a%ptr)
     call c_f_pointer(ptr, diagonal_ptr, [bml_get_N(a)])
     diagonal = diagonal_ptr
+    deallocate(diagonal_ptr)
 
   end subroutine bml_get_diagonal_double_complex
 
@@ -117,6 +121,7 @@ contains
     ptr = bml_get_row_C(a%ptr, i - 1)
     call c_f_pointer(ptr, row_ptr, [bml_get_N(a)])
     row = row_ptr
+    deallocate(row_ptr)
 
   end subroutine bml_get_row_single_real
 
@@ -137,6 +142,7 @@ contains
     ptr = bml_get_row_C(a%ptr, i - 1)
     call c_f_pointer(ptr, row_ptr, [bml_get_N(a)])
     row = row_ptr
+    deallocate(row_ptr)
 
   end subroutine bml_get_row_double_real
 
@@ -157,6 +163,7 @@ contains
     ptr = bml_get_row_C(a%ptr, i - 1)
     call c_f_pointer(ptr, row_ptr, [bml_get_N(a)])
     row = row_ptr
+    deallocate(row_ptr)
 
   end subroutine bml_get_row_single_complex
 
@@ -177,6 +184,7 @@ contains
     ptr = bml_get_row_C(a%ptr, i - 1)
     call c_f_pointer(ptr, row_ptr, [bml_get_N(a)])
     row = row_ptr
+    deallocate(row_ptr)
 
   end subroutine bml_get_row_double_complex
 
