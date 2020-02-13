a484bd307a4ed7f67e3000adcefe6dbdc11c8f75
Nicolas Bock <nicolasbock@gmail.com>
2020-02-13 10:22:15 -0700

    Use our own image for CircleCI
    This will allow us to configure those images as we want to. I am
    hoping that this also means that we can add the Intel compiler for
    example.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

---
+++ refresh-ci-images.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+
+: ${IMAGE_TAG:=bml-ci}
+: ${IMAGE_VERSION:=1.0}
+
+for workflow in build lint; do
+  docker build --tag nicolasbock/${IMAGE_TAG}-${workflow}:${IMAGE_VERSION} ci-images/${workflow}
+  docker push nicolasbock/${IMAGE_TAG}-${workflow}:${IMAGE_VERSION}
+done
