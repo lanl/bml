1bba80538856031c1b9901172916d1b4a4603197
Nicolas Bock <nicolasbock@gmail.com>
2020-03-23 20:13:47 -0600

    Add linting for Python 2 and 3
    This change adds pep8 linting for Python scripts and bashate linting
    for shell scripts.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="bmlscripts",
+    version="0.1",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": [
+            "convert-template = scripts.convert_template:main"
+        ]}
+)
