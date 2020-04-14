d671b727112cc29fc5e66985fc967c3a4e4a9b3c
Daniel Osei-Kuffuor <oseikuffuor1@llnl.gov>
2020-04-14 15:43:03 -0700

    Make csr routines callable from bml interface

--- src/C-interface/bml_getters.c
+++ src/C-interface/bml_getters.c
@@ -39,6 +39,9 @@ bml_get_element(
         case csr:
             return bml_get_csr(A, i, j);
             break;
+        case csr:
+            return bml_get_csr(A, i, j);
+            break;
         default:
             LOG_ERROR("unknown matrix type\n");
             break;
