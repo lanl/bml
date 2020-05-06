586e9d911fff8c261ea09c8b898c7d7b53c6da81
Nicolas Bock <nicolasbock@gmail.com>
2020-05-06 06:31:23 -0600

    Make pushing the CI image optional
    Also stop script on error.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- refresh-ci-images.sh
+++ refresh-ci-images.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-set -x
+set -x -e
 
 : ${IMAGE_TAG:=bml-ci}
 : ${IMAGE_VERSION:=2}
+: ${PUSH_IMAGE:=no}
 
 for workflow in build lint docs; do
   docker build --tag nicolasbock/${IMAGE_TAG}-${workflow}:${IMAGE_VERSION} ci-images/${workflow}
-  docker push nicolasbock/${IMAGE_TAG}-${workflow}:${IMAGE_VERSION}
+  if [[ ${PUSH_IMAGE} = yes ]]; then
+    docker push nicolasbock/${IMAGE_TAG}-${workflow}:${IMAGE_VERSION}
+  fi
 done
