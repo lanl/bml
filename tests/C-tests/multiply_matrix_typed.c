e690d3f172f0b55942318a79bf4031cebb2e7e21
Jean-Luc Fattebert <fattebertj@ornl.gov>
2020-07-24 21:11:10 -0400

    Set various block sizes to test ellblock

--- tests/C-tests/multiply_matrix_typed.c
+++ tests/C-tests/multiply_matrix_typed.c
@@ -1,10 +1,12 @@
 #include "bml.h"
 #include "../macros.h"
 #include "../typed.h"
+#include "ellblock/bml_allocate_ellblock.h"
 
 #include <complex.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 static void TYPED_FUNC(
     ref_multiply) (
@@ -74,6 +76,36 @@ int TYPED_FUNC(
     const bml_matrix_precision_t matrix_precision,
     const int M)
 {
+    // set block sizes for ellblock tests
+    // (unused by other tests)
+    if (matrix_type == ellblock)
+    {
+        int bsizes[N];
+        int count = 0;
+        int nb = 0;
+        for (int i = 0; i < N; i++)
+        {
+            int bsize = (3 + i) % 6 + 1;
+            count += bsize;
+            if (count > N)
+            {
+                bsize -= (count - N);
+                count = N;
+            }
+            bsizes[i] = bsize;
+            if (count == N)
+            {
+                nb = i + 1;
+                break;
+            }
+        }
+        bml_set_block_sizes(bsizes, nb, nb * M / N);
+        for (int i = 0; i < nb; i++)
+        {
+            printf("bsizes[%d] = %d\n", i, bsizes[i]);
+        }
+    }
+
     bml_matrix_t *A = NULL;
     bml_matrix_t *B = NULL;
     bml_matrix_t *C = NULL;
--- tests/C-tests/multiply_matrix_x2_typed.c
+++ tests/C-tests/multiply_matrix_x2_typed.c
@@ -1,9 +1,11 @@
 #include "bml.h"
 #include "../typed.h"
+#include "ellblock/bml_allocate_ellblock.h"
 
 #include <complex.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 static void TYPED_FUNC(
     ref_multiply) (
@@ -72,6 +74,35 @@ int TYPED_FUNC(
     const bml_matrix_precision_t matrix_precision,
     const int M)
 {
+    // set block sizes for ellblock tests
+    // (unused by other tests)
+    if (matrix_type == ellblock)
+    {
+        int bsizes[N];
+        int count = 0;
+        int nb = 0;
+        for (int i = 0; i < N; i++)
+        {
+            int bsize = (5 + i) % 6 + 1;
+            count += bsize;
+            if (count > N)
+            {
+                bsize -= (count - N);
+                count = N;
+            }
+            bsizes[i] = bsize;
+            if (count == N)
+            {
+                nb = i + 1;
+                break;
+            }
+        }
+        bml_set_block_sizes(bsizes, nb, nb * M / N);
+        for (int i = 0; i < nb; i++)
+        {
+            printf("bsizes[%d] = %d\n", i, bsizes[i]);
+        }
+    }
     bml_matrix_t *A = NULL;
     bml_matrix_t *B = NULL;
     bml_matrix_t *C = NULL;
