c4f4ba729848ae9fda1988656b0298099d9f5193
Nicolas Bock <nicolasbock@gmail.com>
2021-04-27 06:57:35 -0600

    Build CI environment in workflow
    This change removes the custom built container from the GitHub
    workflow and instead configures the build environment during the CI
    run itself.
    
    This has the advantage that anyone (including forks) can modify the
    build environment in a pull request and use it for that same pull
    request in CI. In other words a change in the CI environment itself,
    such as a compiler upgrade, can be tested directly in the pull
    request.
    
    In addition, environment secrets [1] for pushing the custom built
    Docker container to Docker Hub are now not necessary anymore, reducing
    the attack surface of the project.
    
    [1] https://docs.github.com/en/actions/reference/encrypted-secrets
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

---
+++ prepare-container.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+set -e -u -x
+
+SUDO=$(which sudo || true)
+
+${SUDO} apt-get update
+${SUDO} apt-get install --assume-yes --no-install-recommends \
+  apt-transport-https \
+  ca-certificates \
+  gnupg \
+  wget
+
+cat <<EOF | ${SUDO} tee /etc/apt/sources.list.d/clang.list
+# i386 not available
+deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic main
+# deb-src http://apt.llvm.org/bionic/ llvm-toolchain-bionic main
+# 11
+deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-11 main
+# deb-src http://apt.llvm.org/bionic/ llvm-toolchain-bionic-11 main
+# 12
+deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-12 main
+# deb-src http://apt.llvm.org/bionic/ llvm-toolchain-bionic-12 main
+EOF
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | ${SUDO} apt-key add -
+
+cat <<EOF | ${SUDO} tee /etc/apt/sources.list.d/cmake.list
+deb http://ppa.launchpad.net/janisozaur/cmake-update-bionic/ubuntu bionic main
+# deb-src http://ppa.launchpad.net/janisozaur/cmake-update-bionic/ubuntu bionic main
+EOF
+${SUDO} apt-key adv --keyserver keyserver.ubuntu.com \
+  --recv-keys DBA92F17B25AD78F9F2D9F713DEC686D130FF5E4
+
+cat <<EOF | ${SUDO} tee /etc/apt/sources.list.d/toolchain.list
+deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu bionic main
+# deb-src http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu bionic main
+EOF
+${SUDO} apt-key adv --keyserver keyserver.ubuntu.com \
+  --recv-keys 60C317803A41BA51845E371A1E9377A2BA9EF27F
+
+cat <<EOF | ${SUDO} tee /etc/apt/sources.list.d/emacs.list
+deb http://ppa.launchpad.net/kelleyk/emacs/ubuntu bionic main
+# deb-src http://ppa.launchpad.net/kelleyk/emacs/ubuntu bionic main
+EOF
+${SUDO} apt-key adv --keyserver keyserver.ubuntu.com \
+  --recv-keys 873503A090750CDAEB0754D93FF0E01EEAAFC9CD
+
+${SUDO} apt-get update
+${SUDO} ln -fs /usr/share/zoneinfo/UTC /etc/localtime
+${SUDO} apt-get install --assume-yes tzdata
+DEBIAN_FRONTEND=noninteractive ${SUDO} dpkg-reconfigure \
+  --frontend noninteractive tzdata
+
+${SUDO} apt-get install --assume-yes --no-install-recommends \
+  apt-utils \
+  bundler \
+  emacs27 \
+  build-essential \
+  cmake cmake-data \
+  clang-9 llvm-9-dev libomp-9-dev \
+  gcc-4.8 g++-4.8 gfortran-4.8 \
+  gcc-9 g++-9 gfortran-9 \
+  gcc-10 g++-10 gfortran-10 \
+  git-core \
+  indent \
+  openssh-client \
+  libblas-dev liblapack-dev \
+  mpich libmpich-dev \
+  python python-pip python-wheel \
+  texlive \
+  valgrind \
+  vim
