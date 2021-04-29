a6693c9d7c84e9628c56693cdda8657c3656eae2
Nicolas Bock <nicolasbock@gmail.com>
2021-04-29 06:33:20 -0600

    Start testing with gcc-11
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- prepare-container.sh
+++ prepare-container.sh
@@ -67,6 +67,7 @@ ${SUDO} apt-get install --assume-yes --no-install-recommends \
   gcc-4.8 g++-4.8 gfortran-4.8 \
   gcc-9 g++-9 gfortran-9 \
   gcc-10 g++-10 gfortran-10 \
+  gcc-11 g++-11 gfortran-11 \
   git-core \
   indent \
   openssh-client \
