45092410782f5eb30bc39009f9fb201b92383310
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-07-11 22:03:59 +0200

    Initial workflow for GCC toolchain on OSX

---
+++ scripts/ci-gcc-5-test-1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -u -x
+
+basedir=$(readlink --canonicalize $(dirname $0)/..)
+
+[[ -f ${basedir}/scripts/ci-defaults.sh ]] && . ${basedir}/scripts/ci-defaults.sh
+
+export CC=gcc-5
+export CXX=g++-5
+export FC=gfortran-5
+export BUILD_SHARED_LIBS=yes
+export BML_OPENMP=no
+export BML_INTERNAL_BLAS=no
+
+${basedir}/build.sh testing
---
+++ scripts/prepare-homebrew.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+brew install \
+  coreutils \
+  cmake \
+  gcc@5 \
+  gcc@11 \
+  openblas
