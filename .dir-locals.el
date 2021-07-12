012fa6a140b95fef61db1e2f31aae5ee207e4f23
Nicolas Bock <nicolasbock@gmail.com>
2021-07-12 21:21:10 +0200

    Fix missing build targets
    Commit 72338c2750af601e9af3256adfdacde040a69e38 introduced a
    regression which broke the `install` verb of the `build.sh` script.
    
    Running `make install` implies building the library whereas `cmake
    --install` does not and expects the project to be built.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c-mode
-  ((c-basic-offset . 4)
-   (c-offsets-alist . ((arglist-intro . 4)))))
- (f90-mode
-  ((f90-if-indent . 2)
-   (f90-do-indent . 2)
-   (f90-type-indent . 2))))
+((c-mode . (((c-basic-offset . 4) . ((c-offsets-alist
+                                      (arglist-intro . 4))))))
+ (f90-mode . (((f90-if-indent . 2) . ((f90-do-indent . 2)
+                                      (f90-type-indent . 2)))))
+ (sh-mode . ((sh-basic-offset . 4))))

lanl/bml

commit 72338c2750af601e9af3256adfdacde040a69e38

Allow pass-through CMake arguments in build script

build.sh
@@ -83,6 +83,7 @@ EOF
     echo "BML_ELLBLOCK_MEMPOOL   Use ellblock memory pool    (default is ${BML_ELLBLOCK_MEMPOOL}"
     echo "CUDA_TOOLKIT_ROOT_DIR  Path to CUDA dir            (default is ${CUDA_TOOLKIT_ROOT_DIR})"
     echo "INTEL_OPT              {yes, no}                   (default is ${INTEL_OPT})"
+    echo "CMAKE_ARGS             pass-through CMake flags    (default is ${CMAKE_ARGS})"
 }
 
 set_defaults() {
@@ -125,6 +126,7 @@ set_defaults() {
     : ${BML_ELLBLOCK_MEMPOOL:=no}
     : ${CUDA_TOOLKIT_ROOT_DIR:=}
     : ${INTEL_OPT:=no}
+    : ${CMAKE_ARGS:=}
 }
 
 die() {
@@ -209,18 +211,19 @@ configure() {
         -DBML_ELLBLOCK_MEMPOOL="${BML_ELLBLOCK_MEMPOOL}" \
         -DCUDA_TOOLKIT_ROOT_DIR="${CUDA_TOOLKIT_ROOT_DIR}" \
         -DINTEL_OPT="${INTEL_OPT:=no}" \
+        ${CMAKE_ARGS} \
         | tee --append "${LOG_FILE}"
     check_pipe_error
     cd "${TOP_DIR}"
 }
 
 compile() {
-    make -C "${BUILD_DIR}" | tee --append "${LOG_FILE}"
+    cmake --build "${BUILD_DIR}" | tee --append "${LOG_FILE}"
     check_pipe_error
 }
 
 docs() {
-    make -C "${BUILD_DIR}" docs 2>&1 | tee --append "${LOG_FILE}"
+    cmake --build -C "${BUILD_DIR}" --target docs 2>&1 | tee --append "${LOG_FILE}"
     check_pipe_error
     #make -C "${BUILD_DIR}/doc/latex" 2>&1 | tee -a "${LOG_FILE}"
     #check_pipe_error
@@ -230,7 +233,7 @@ docs() {
 }
 
 install() {
-    make -C "${BUILD_DIR}" install 2>&1 | tee --append "${LOG_FILE}"
+    cmake --install "${BUILD_DIR}" 2>&1 | tee --append "${LOG_FILE}"
     check_pipe_error
 }
 
@@ -284,7 +287,7 @@ tags() {
 }
 
 dist() {
-    make -C "${BUILD_DIR}" dist 2>&1 | tee --append "${LOG_FILE}"
+    cmake --build "${BUILD_DIR}" --target dist 2>&1 | tee --append "${LOG_FILE}"
     check_pipe_error
 }
 

commit 012fa6a140b95fef61db1e2f31aae5ee207e4f23

Fix missing build targets
Commit 72338c2750af601e9af3256adfdacde040a69e38 introduced a
regression which broke the `install` verb of the `build.sh` script.

Running `make install` implies building the library whereas `cmake
--install` does not and expects the project to be built.

Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

.dir-locals.el
@@ -1,10 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c-mode
-  ((c-basic-offset . 4)
-   (c-offsets-alist . ((arglist-intro . 4)))))
- (f90-mode
-  ((f90-if-indent . 2)
-   (f90-do-indent . 2)
-   (f90-type-indent . 2))))
+((c-mode . (((c-basic-offset . 4) . ((c-offsets-alist
+                                      (arglist-intro . 4))))))
+ (f90-mode . (((f90-if-indent . 2) . ((f90-do-indent . 2)
+                                      (f90-type-indent . 2)))))
+ (sh-mode . ((sh-basic-offset . 4))))

build.sh
@@ -10,6 +10,7 @@ if (( $? != 0 )); then
     TOP_DIR=$(pwd -P $TOP_DIR)
 fi
 
+: ${CMAKE:=cmake}
 : ${BUILD_DIR:=${TOP_DIR}/build}
 : ${INSTALL_DIR:=${TOP_DIR}/install}
 : ${PARALLEL_TEST_JOBS:=1}
@@ -172,7 +173,7 @@ configure() {
     if [[ -f "${BUILD_DIR}/CMakeCache.txt" ]]; then
         rm -v "${BUILD_DIR}/CMakeCache.txt" || die
     fi
-    ${CMAKE:=cmake} .. \
+    ${CMAKE} .. \
         -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
         -DCMAKE_C_COMPILER="${CC}" \
         -DCMAKE_CXX_COMPILER="${CXX}" \
@@ -220,12 +221,12 @@ configure() {
 }
 
 compile() {
-    cmake --build "${BUILD_DIR}" | tee --append "${LOG_FILE}"
+    ${CMAKE} --build "${BUILD_DIR}" | tee --append "${LOG_FILE}"
     check_pipe_error
 }
 
 docs() {
-    cmake --build "${BUILD_DIR}" --target docs 2>&1 | tee --append "${LOG_FILE}"
+    ${CMAKE} --build "${BUILD_DIR}" --target docs 2>&1 | tee --append "${LOG_FILE}"
     check_pipe_error
     #make -C "${BUILD_DIR}/doc/latex" 2>&1 | tee -a "${LOG_FILE}"
     #check_pipe_error
@@ -235,16 +236,16 @@ docs() {
 }
 
 install() {
-    cmake --install "${BUILD_DIR}" 2>&1 | tee --append "${LOG_FILE}"
+    ${CMAKE} --build "${BUILD_DIR}" --target install 2>&1 | tee --append "${LOG_FILE}"
     check_pipe_error
 }
 
 testing() {
     cd "${BUILD_DIR}"
     ctest --output-on-failure \
-      --parallel ${PARALLEL_TEST_JOBS} \
-      ${TESTING_EXTRA_ARGS} \
-      2>&1 | tee --append "${LOG_FILE}"
+        --parallel ${PARALLEL_TEST_JOBS} \
+        ${TESTING_EXTRA_ARGS} \
+        2>&1 | tee --append "${LOG_FILE}"
     check_pipe_error
 
     # Get skipped tests and re-run them with verbose output.
@@ -254,11 +255,11 @@ testing() {
         echo "Found skipped tests: ${SKIPPED_TESTS[*]}"
         local skipped
         for skipped in "${SKIPPED_TESTS[@]}"; do
-          echo "Re-running skipped test ${skipped}"
-          ctest --verbose \
-            ${TESTING_EXTRA_ARGS} \
-            --tests-regex "${skipped}" \
-            2>&1 | tee --append "${LOG_FILE}"
+            echo "Re-running skipped test ${skipped}"
+            ctest --verbose \
+                ${TESTING_EXTRA_ARGS} \
+                --tests-regex "${skipped}" \
+                2>&1 | tee --append "${LOG_FILE}"
         done
     fi
     cd "${TOP_DIR}"
@@ -289,7 +290,7 @@ tags() {
 }
 
 dist() {
-    cmake --build "${BUILD_DIR}" --target dist 2>&1 | tee --append "${LOG_FILE}"
+    ${CMAKE} --build "${BUILD_DIR}" --target dist 2>&1 | tee --append "${LOG_FILE}"
     check_pipe_error
 }
 