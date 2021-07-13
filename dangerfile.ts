db42cf4005c8e49979910012c7dd09c3619235c7
Nicolas Bock <nicolasbock@gmail.com>
2021-07-13 09:23:09 +0200

    FIXUP

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,25 +1,5 @@
-# Check basic commit message formatting
-#
-# vim: syntax=ruby
+// Check basic commit message formatting
 
-# Lint the commit message.
-commit_lint.check fail: :all
+import { danger } from "danger"
 
-# Ensure a clean commit history.
-if git.commits.any? { |c| c.message =~ /^Merge branch/ }
-  fail('Please rebase to get rid of the merge commits in this PR')
-end
-
-# Look for prose issues.
-# prose.lint_files
-
-# Running plugin with reviewers count specified. Find maximum two
-# reviewers.
-mention.run(2, [], [])
-
-# Show the greeting.
-welcome_message.greet
-
-# Local Variables:
-# mode: ruby
-# End:
+const pr = danger.github.pr
