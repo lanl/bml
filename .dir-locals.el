69d601c6a22e8dfc532c8902fd52e28b12e1db01
Nicolas Bock <nicolasbock@gmail.com>
2021-07-13 07:52:35 +0200

    Fix dir local variables
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,7 @@
 
 ((c-mode . (((c-basic-offset . 4) . ((c-offsets-alist
                                       (arglist-intro . 4))))))
- (f90-mode . (((f90-if-indent . 2) . ((f90-do-indent . 2)
-                                      (f90-type-indent . 2)))))
+ (f90-mode . ((f90-type-indent . 2)
+              (f90-do-indent . 2)
+              (f90-if-indent . 2)))
  (sh-mode . ((sh-basic-offset . 4))))
