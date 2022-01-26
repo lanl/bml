84e45a7ae8950c7da2d561adb70300b0a6f16f1d
Nicolas Bock <nicolasbock@gmail.com>
2022-01-26 07:03:05 -0700

    Rename README.md to README.rst
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, bml-authors'
 author = 'bml-authors'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.2.3'
+release = 'v2.1.2'
 
 
 # -- General configuration ---------------------------------------------------
@@ -47,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
