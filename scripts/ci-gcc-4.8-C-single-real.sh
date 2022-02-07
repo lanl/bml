116de2f1fee09f355279a7144db9b4c17670fdc0
Nicolas Bock <nicolasbock@gmail.com>
2022-02-07 09:35:38 -0700

    Clean up CI tests
    This change updates the gcc-4.8 tests to use the same naming convention and test
    settings as the gcc-11 tests.
    
    The change also adds OPENMP for the Fortran tests.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- scripts/ci-gcc-4.8-C-single-real.sh
+++ scripts/ci-gcc-4.8-C-single-real.sh
@@ -7,9 +7,11 @@ basedir=$(readlink --canonicalize $(dirname $0)/..)
 export CC=${CC:-gcc-4.8}
 export CXX=${CXX:-g++-4.8}
 export FC=${FC:-gfortran-4.8}
-export BUILD_SHARED_LIBS=${BUILD_SHARED_LIBS:-yes}
+export BUILD_SHARED_LIBS=${BUILD_SHARED_LIBS:-no}
 export BML_OPENMP=${BML_OPENMP:-no}
 export BML_INTERNAL_BLAS=${BML_INTERNAL_BLAS:-no}
+export TESTING_EXTRA_ARGS=${TESTING_EXTRA_ARGS:-"-R C-.*-single_real"}
+export BML_VALGRIND=${BML_VALGRIND:-yes}
 
 [[ -f ${basedir}/scripts/ci-defaults.sh ]] && . ${basedir}/scripts/ci-defaults.sh
 
