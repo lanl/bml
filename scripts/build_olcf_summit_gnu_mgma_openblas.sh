d1d92aa2544888b26cf9c90788dd443b9d31ab7f
Jean-Luc Fattebert <fattebertj@ornl.gov>
2022-03-15 11:03:10 -0400

    Clean up build script with magma on Summit

--- scripts/build_olcf_summit_gnu_mgma_openblas.sh
+++ scripts/build_olcf_summit_gnu_mgma_openblas.sh
@@ -2,7 +2,7 @@
 module load cmake
 module load cuda
 module load gcc/9.3.0
-module load netlib-lapack
+#lapack included in openblas
 module load openblas
 module load magma
 
@@ -19,16 +19,13 @@ export MAGMA_ROOT=${OLCF_MAGMA_ROOT:="${OLCF_MAGMA_ROOT}"}
 export CC=${CC:=gcc}
 export FC=${FC:=gfortran}
 export CXX=${CXX:=g++}
-export BLAS_VENDOR=${BLAS_VENDOR:=GNU}
+export BLAS_VENDOR=${BLAS_VENDOR:=OpenBLAS}
 export BML_OPENMP=${BML_OPENMP:=yes}
 export BML_OMP_OFFLOAD=${BML_OMP_OFFLOAD:=no}
 export INSTALL_DIR=${INSTALL_DIR:="${MY_PATH}/install"}
 export BML_TESTING=${BML_TESTING:=yes}
 export BML_MAGMA=${BML_MAGMA:=yes}
-export BML_COMPLEX=${BML_COMPLEX:=yes}
 export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Release}
-export EXTRA_CFLAGS=${EXTRA_CFLAGS:="-fopenmp"}
-export BML_ELLBLOCK_MEMPOOL=${BML_ELLBLOCK_MEMPOOL:=no}
 
 #use jsrun to run tests on a compute node
 export BML_NONMPI_PRECOMMAND=${BML_NONMPI_PRECOMMAND:=${JSRUN}}
