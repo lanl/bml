cf0d056deb817c38c1b72ac5b6e676add600405d
Jamaludin Mohd Yusof <jamal@lanl.gov>
2023-03-14 17:10:27 +0000

    Remove hard-coded paths from flags

--- scripts/build_mkl_gpu.sh
+++ scripts/build_mkl_gpu.sh
@@ -17,7 +17,7 @@ export FC=ifx
 export CXX=icpx
 export MKL_GPU=yes
 
-export BLAS_VENDOR=${BLAS_VENDOR:=MKL}
+export BLAS_VENDOR=${BLAS_VENDOR:=Intel10_64ilp}
 export BML_OPENMP=${BML_OPENMP:=yes}
 export BML_CUDA=${BML_CUDA:=no}
 export MKL_GPU=${MKL_GPU:=yes}
@@ -33,7 +33,8 @@ export CUDA_TOOLKIT_ROOT_DIR=""
 export CMAKE_Fortran_FLAGS="-fiopenmp -fopenmp-targets=spir64 -DINTEL_SDK"
 export CMAKE_C_FLAGS="-O3 -fiopenmp -fopenmp-targets=spir64 -D__STRICT_ANSI__ -DINTEL_SDK"
 
-export EXTRA_LINK_FLAGS="-L/soft/restricted/CNDA/sdk/2021.04.30.001/oneapi/mkl/2021u3_20210525/lib/intel64 -lmkl_lapack95_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -lpthread -lm -lmkl_intel_ilp64  -lmkl_sycl -lstdc++ -Wl,-rpath,/soft/restricted/CNDA/sdk/2021.04.30.001/oneapi/kokkos/20210323-3.1/../../compiler/latest/linux/compiler/lib/intel64 -L/soft/restricted/CNDA/sdk/2021.04.30.001/oneapi/kokkos/20210323-3.1/../../compiler/latest/linux/compiler/lib/intel64 -liomp5 -lsycl -lOpenCL -lm -lpthread -ldl"
+# export EXTRA_LINK_FLAGS="-L$MKL_ROOT/lib/intel64 -lmkl_lapack95_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -lpthread -lm -lmkl_intel_ilp64  -lmkl_sycl -lstdc++ -Wl,-rpath,$MKL_ROOT/lib/intel64 -L$MKL_ROOT/lib/intel64 -liomp5 -lsycl -lOpenCL -lm -lpthread -ldl"
+export EXTRA_LINK_FLAGS=" -lmkl_lapack95_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -lpthread -lm -lmkl_intel_ilp64  -lmkl_sycl -lstdc++ -liomp5 -lsycl -lOpenCL -lm -lpthread -ldl"
 
 ./build.sh configure
 #./build.sh install
