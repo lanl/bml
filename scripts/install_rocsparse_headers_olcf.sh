0c3819ab9f0a8d1d2776d1d3dd301784c7c7b1e0
Mike Wall <mewall@lanl.gov>
2023-06-13 10:50:27 -0400

    Add script to install rocsparse headers on OLCF machines

---
+++ scripts/install_rocsparse_headers_olcf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+this_dir=$(pwd)
+if [[ ! -e src/C-interface || $this_dir == *build ]]; then
+	echo "Please run this script in the bml/ root directory"
+	exit 1
+fi
+cp -r $ROCM_PATH/include/rocsparse src/C-interface/.
+sed -i -e 's/\[\[.*//' src/C-interface/rocsparse/rocsparse-functions.h 
+sed -i -e 's/.*\]\] ROCSPARSE_EXPORT/ROCSPARSE_EXPORT/' src/C-interface/rocsparse/rocsparse-functions.h
