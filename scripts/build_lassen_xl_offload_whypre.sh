393f5ad7685d08b3423e70ee58b57a7334b4c30d
Daniel Osei-Kuffuor <oseikuffuor1@llnl.gov>
2024-03-20 10:02:58 -0700

    Enable BML to use hypre's GPU-capable sparse matrix routines
    * Enables sparse matrix multiplication and addition on GPUs
    * Uses openMP offload to transfer BML data to hypre
    * Current implementation supports NVIDIA GPUs only
    * Supports only one precision build of hypre (single  of double)

--- scripts/build_lassen_xl_offload_whypre.sh
+++ scripts/build_lassen_xl_offload_whypre.sh
@@ -2,20 +2,25 @@
 
 # Make sure all the paths are correct
 
-source setenv_lassen_offload.sh
+source scripts/setenv_lassen_offload.sh
 
 rm -r build
 rm -r install
 
 MY_PATH=$(pwd)
 
-export CC=${CC:=xlc-gpu}
+HYPRE_INSTALL_PATH="/usr/WS1/osei/soft/CoPA/lassen/gpu/fork/bml-hypre/hypre/src/hypre"
+
+export CC=${CC:=xlc++-gpu}
 export FC=${FC:=xlf2003-gpu}
 export CXX=${CXX:=xlc++-gpu}
 export BLAS_VENDOR=${BLAS_VENDOR:=Auto}
 export BML_OPENMP=${BML_OPENMP:=yes}
 export BML_OMP_OFFLOAD=${BML_OMP_OFFLOAD:=yes}
-export BML_CUSPARSE=${BML_CUSPARSE:=yes}
+export BML_OFFLOAD_ARCH=${BML_OFFLOAD_ARCH:=NVIDIA}
+export BML_CUSPARSE=${BML_CUSPARSE:=no}
+export BML_HYPRE=${BML_HYPRE:=yes}
+export HYPRE_ROOT=${HYPRE_INSTALL_PATH}
 export BML_COMPLEX=${BML_COMPLEX:=no}
 export INSTALL_DIR=${INSTALL_DIR:="${MY_PATH}/install"}
 export BML_TESTING=${BML_TESTING:=yes}
@@ -23,7 +28,10 @@ export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Release}
 export EXTRA_CFLAGS=${EXTRA_CFLAGS:=""}
 export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:=""}
 #export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:="-lm -L/usr/tce/packages/xl/xl-2021.03.11/xlC/16.1.1/lib -libmc++"}
-export BLAS_LIBRARIES=${BLAS_LIBRARIES:="-L${LAPACK_DIR} -llapack -lblas"}
+export BLAS_LIBRARIES=${BLAS_LIBRARIES:="-L${ESSLLIBDIR64} -lesslsmp"}
+export LAPACK_LIBRARIES=${LAPACK_LIBRARIES:="-L${LAPACK_DIR} -llapack"}
+
+export CUDA_TOOLKIT_ROOT_DIR=${CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME}
 
 ./build.sh configure
 
