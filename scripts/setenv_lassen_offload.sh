36b652226f2d8af15467fa1beec6f0bdf64351a8
Daniel Osei-Kuffuor <oseikuffuor1@llnl.gov>
2024-03-20 10:02:58 -0700

    Enable BML to use hypre's GPU-capable sparse matrix routines
    * Enables sparse matrix multiplication and addition on GPUs
    * Uses openMP offload to transfer BML data to hypre
    * Current implementation supports NVIDIA GPUs only
    * Supports only one precision build of hypre (single  of double)

--- scripts/setenv_lassen_offload.sh
+++ scripts/setenv_lassen_offload.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 #module purge
-module load cmake
-module load xl/2021.03.11-cuda-11.2.0
-module load cuda/11.2.0
+module load cmake/3.23.1
+module load xl/2022.03.10-cuda-11.8.0
+module load cuda/11.8.0
 module load lapack/3.9.0-xl-2020.11.12
-#module load essl
-export CUDA_TOOLKIT_ROOT_DIR=${CUDA_TOOLKIT_ROOT_DIR="/usr/tce/packages/cuda/cuda-11.2.0"}
-
+module load essl
