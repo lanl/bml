bda9a9ae8bc8564145a696b0e6ee8dcf359e6c1b
Nicolas Bock <nicolasbock@gmail.com>
2025-02-04 11:06:59 -0700

    Update compiler for OS X runner (#762)
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- scripts/ci-gcc-14-C-single-real.sh
+++ scripts/ci-gcc-14-C-single-real.sh
@@ -10,13 +10,13 @@ done
 
 basedir=$(${READLINK} --canonicalize $(dirname $0)/..)
 
-export CC=${CC:-gcc-11}
-export CXX=${CXX:-g++-11}
-export FC=${FC:-gfortran-11}
+export CC=${CC:-gcc-14}
+export CXX=${CXX:-g++-14}
+export FC=${FC:-gfortran-14}
 export BUILD_SHARED_LIBS=${BUILD_SHARED_LIBS:-no}
 export BML_OPENMP=${BML_OPENMP:-no}
 export BML_INTERNAL_BLAS=${BML_INTERNAL_BLAS:-no}
-export TESTING_EXTRA_ARGS=${TESTING_EXTRA_ARGS:-"-R C-.*-single_complex"}
+export TESTING_EXTRA_ARGS=${TESTING_EXTRA_ARGS:-"-R C-.*-single_real"}
 export BML_VALGRIND=${BML_VALGRIND:-yes}
 
 [[ -f ${basedir}/scripts/ci-defaults.sh ]] && . ${basedir}/scripts/ci-defaults.sh
